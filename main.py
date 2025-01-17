cb60d2ce439bc855bd03202d14c3d4e342f33ab9
Sunga Thawethe <sungabt@gmail.com>
2025-01-17 17:19:09 +0200

    Return books and update db successfully

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import requests
+from datetime import datetime
 
 class LibraryApp(tk.Tk):
   def __init__(self):
@@ -35,83 +36,6 @@ class LibraryApp(tk.Tk):
     else:
       self.result_label.config(text="Book not found")
 
-  
-  def checkout_book(self):
-    
-    # is book.status avaliable? 
-    # => get request for the book.avaliability
-    barcode = self.barcode_entry.get()
-    response = requests.get(f'http://localhost:3000/api/v1/books/barcode/{barcode}')
-    if response.status_code == 200:
-
-      book = response.json()
-      self.book_id = book['id']
-      
-      if book['avaliability'] == 'avaliable':
-        
-        # self.status_label = tk.Label(self, text="Choose the new book status")
-        # self.status_label.pack()
-        
-        # status_options = ["Avaliable", "Checked Out", "Unavaliable"]
-        
-        # self.new_status = tk.StringVar(self)
-        # self.new_status.set(status_options[0])
-        
-        # status_dropdown = tk.OptionMenu(self, self.new_status, *status_options)
-        # status_dropdown.pack()
-        
-        self.status = 'Checked Out'
-    
-        
-        self.school_id_label = tk.Label(self, text="Enter the School ID number")
-        self.school_id_label.pack()
-      
-        self.school_id_entry = tk.Entry(self)
-        self.school_id_entry.pack()
-        self.school_id_entry.bind("<Return>", self.search_user)
-
-        self.id_result_label = tk.Label(self, text="")
-        self.id_result_label.pack()
-        
-        self.confirm_checkout = tk.Button(self, text='Confirm', command=self.post_activity)
-        self.confirm_checkout.pack()
-        
-      else:
-        self.status_label = tk.Label(self, text="Book is not avaliable")
-        self.status_label.pack()
-    
-        
-  def post_activity(self):
-    activity_data = {
-      'user_id': self.user_id,
-      'book_id': self.book_id,
-      'status': self.status
-    }
-    
-    print(activity_data)
-    
-    activity_response = requests.post('http://localhost:3000/api/v1/activities', json=activity_data)
-    
-    if activity_response.status_code == 201:
-      print("Check Out Successful!")
-      
-      # self.put_book
-    else:
-      print("Error in checkout", activity_response.json())
-          
-  def put_book(self):       
-    book_data = {
-      'avaliability': self.new_status
-    }
-    
-    book_response = requests.put(f'http://localhost:3000/api/v1/books/{self.book_id}', json=book_data)
-    
-    if book_response.status_code == 201: 
-      print("Book Status updated Successfully!")
-    else:
-      print("Error in Update", book_response.json())
-    
-
   def search_user(self, event): 
     school_id = self.school_id_entry.get()
     response = requests.get(f'http://localhost:3000/api/v1/users/school_id/{school_id}')
@@ -127,15 +51,151 @@ class LibraryApp(tk.Tk):
       print(self.user_id)
       self.id_result_label.config(text=f"User: {user['name']}. Role: {user['role']}. System ID: {user['id']}.")
     else:
+      # no: 
+      # => return error message 
       self.id_result_label.config(text="User not found")
+      
+  def search_activity(self, event=None): 
+      book_id = self.book_id
+      user_id = self.user_id
+      
+      response = requests.get(f'http://localhost:3000/api/v1/activities/ids/{book_id}/{user_id}')
+      
+      if response.status_code == 200:
+          activity = response.json()
+          self.activity_id = activity['id']
+          print(self.activity_id)
+          self.activity_id_label.config(text=f"Checkout ID number is:  {activity['id']}.")
+      else:
+          print(response.status_code)
+          self.activity_id_label.config(text="Entry not found")
+      
+  def checkout_book(self):
     
-    # no: 
-    # => return error message 
+    # is book.status avaliable? 
+    # => get request for the book.avaliability
+    barcode = self.barcode_entry.get()
+    response = requests.get(f'http://localhost:3000/api/v1/books/barcode/{barcode}')
+    if response.status_code == 200:
+
+      book = response.json()
+      self.book_id = book['id']
+      
+      if book['avaliability'] == 'avaliable':
+        
+        self.status = 'Checked Out'
+        
+        self.school_id_label = tk.Label(self, text="Enter the School ID number")
+        self.school_id_label.pack()
+      
+        self.school_id_entry = tk.Entry(self)
+        self.school_id_entry.pack()
+        self.school_id_entry.bind("<Return>", self.search_user)
+
+        self.id_result_label = tk.Label(self, text="")
+        self.id_result_label.pack()
+        
+        self.confirm_checkout = tk.Button(self, text='Confirm', command=self.checkout_activity)
+        self.confirm_checkout.pack()
+        
+      else:
+        self.status_label = tk.Label(self, text="Book is not avaliable")
+        self.status_label.pack()
+    
+        
+  def checkout_activity(self):
+    activity_data = {
+      'user_id': self.user_id,
+      'book_id': self.book_id,
+      'status': self.status
+    }
+    
+    print(activity_data)
+    
+    activity_response = requests.post('http://localhost:3000/api/v1/activities', json=activity_data)
+    
+    book_update_data = {
+      'avaliability': 'Checked Out'
+    }
+    
+    book_response = requests.put(f'http://localhost:3000/api/v1/books/{self.book_id}', json=book_update_data)
+        
+    if activity_response.status_code == 201 and book_response.status_code == 200:
+      print("Check Out Successful!")
+      print("Book is now awaiting Return!")
+    else:
+      print("Error in checkout or book update") 
+      print(activity_response.json())
+      print(book_response.json()) 
+
 
 
   def return_book(self):
-  
-    pass
+    # is book.status unavaliable? 
+    # => get request for the book.avaliability
+    barcode = self.barcode_entry.get()
+    response = requests.get(f'http://localhost:3000/api/v1/books/barcode/{barcode}')
+    if response.status_code == 200:
+
+      book = response.json()
+      self.book_id = book['id']
+      
+      if book['avaliability'] == 'Checked Out':
+        
+        self.status = 'Checked In'
+      
+        self.school_id_label = tk.Label(self, text="Enter the School ID number")
+        self.school_id_label.pack()
+      
+        self.school_id_entry = tk.Entry(self)
+        self.school_id_entry.pack()
+        self.school_id_entry.bind("<Return>", self.search_user)
+
+        self.id_result_label = tk.Label(self, text="")
+        self.id_result_label.pack()
+        
+        self.check_entry = tk.Button(self, text='Check Entry Number', command=self.search_activity)
+        self.check_entry.pack()
+        
+        self.activity_id_label = tk.Label(self, text="")
+        self.activity_id_label.pack()
+        
+        self.confirm_return = tk.Button(self, text='Confirm', command=self.checkin_activity)
+        self.confirm_return.pack()
+        
+      else:
+        self.status_label = tk.Label(self, text="Book is already returned.")
+        self.status_label.pack()
+        
+        
+   
+  def checkin_activity(self):
+    
+    now =  datetime.now()
+    
+    update_data = {
+      'user_id': self.user_id,
+      'book_id': self.book_id,
+      'status': self.status,
+      'checkin': now.strftime("%Y-%m-%dT%H:%M:%S")
+    }
+    
+    print(update_data)
+    activity_response = requests.put(f'http://localhost:3000/api/v1/activities/{self.activity_id}', json=update_data)
+    
+    book_update_data = {
+      'avaliability': 'avaliable'
+    }
+    
+    book_response = requests.put(f'http://localhost:3000/api/v1/books/{self.book_id}', json=book_update_data)
+        
+    if activity_response.status_code == 200 and book_response.status_code == 200:
+      print("Check In Successful!")
+      print("Book is now avaliable for Check Out!")
+    else:
+      print("Error in checkin or book update")   
+
+        
   
 if __name__ == "__main__":
   app = LibraryApp()
